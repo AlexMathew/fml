044ca2f7ee43deb94f940ef1b5d30bb9369cb958
Alex Mathew <alexmathew003@gmail.com>
2019-02-09 15:46:34 +0530

    Add mutation to create user

--- fml/schema.py
+++ fml/schema.py
@@ -1,4 +1,5 @@
 import graphene
+import graphql_jwt
 
 import players.schema
 
@@ -10,4 +11,13 @@ class Query(
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(
+    players.schema.Mutation,
+    graphene.ObjectType,
+):
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
--- players/schema.py
+++ players/schema.py
@@ -2,6 +2,7 @@ import django_filters
 import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
+from django.contrib.auth.models import User
 
 from .models import Player
 
@@ -21,3 +22,28 @@ class PlayerNode(DjangoObjectType):
 class Query(graphene.ObjectType):
     player = graphene.relay.Node.Field(PlayerNode)
     players = DjangoFilterConnectionField(PlayerNode, filterset_class=PlayerFilter)
+
+
+class CreatePlayer(graphene.relay.ClientIDMutation):
+    player = graphene.Field(PlayerNode)
+
+    class Input:
+        username = graphene.String()
+        email = graphene.String()
+
+    def mutate_and_get_payload(self, info, **input):
+        user = User(
+            username=input.get('username'),
+            email=input.get('email'),
+        )
+        user.save()
+        player = Player(
+            user=user,
+        )
+        player.save()
+
+        return CreatePlayer(player=player)
+
+
+class Mutation(graphene.AbstractType):
+    create_player = CreatePlayer.Field()
