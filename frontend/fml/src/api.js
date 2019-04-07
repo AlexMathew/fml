14c40bdf204ae10ae2794c55e08c8d374253de6d
Alex Mathew <alexmathew003@gmail.com>
2019-04-07 10:47:58 +0530

    Move frontend to a separate repo

--- frontend/fml/src/api.js
+++
@@ -1,5 +0,0 @@
-export let API_URL = "";
-
-if (process.env.NODE_ENV === "development") {
-  API_URL = "http://localhost:8000/graphql/";
-}
--- frontend/fml/src/constants.js
+++
@@ -1,3 +0,0 @@
-const BASE_PREFIX = process.env.NODE_ENV === "development" ? "DEV_" : "";
-
-export const AUTH_TOKEN_FIELD = BASE_PREFIX + "FML_AUTH_TOKEN";
